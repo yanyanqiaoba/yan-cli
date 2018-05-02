d0b9141db92d4bfb194ae5f082cdf0467b07b574
fenghaiyan <hfeng1@xiaohongshu.com>
2018-05-02 17:10:23 +0800

    yan-cli init

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+const program = require('commander'); //自定义命令
+const chalk = require('chalk'); //颜色插件
+const pkg = require('../package.json');
+
+const newCli = require('../lib/new');
+
+program.version(pkg.version)
+  .option('new', 'init RED Design QA Demo')
+  .parse(process.argv);
+
+if (program.new) {
+  newCli(process.argv);
+}
